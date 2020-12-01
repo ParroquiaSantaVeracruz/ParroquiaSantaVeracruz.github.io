8f9f589325659d37d29aeabc8aea9e57289d5022
ParroquiaSantaVeracruz <74343671+ParroquiaSantaVeracruz@users.noreply.github.com>
2020-12-01 09:52:52 -0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "8b9c151e37b52a959d61075b7ccf8107",
+  "assets/AssetManifest.json": "6d3fb33e3e0d5c1e59c58c4311b06092",
 "assets/assets/fuentes/FredokaOne-Regular.ttf": "4a2f2ea45a0bb1abe81b47d0afde4aae",
 "assets/assets/fuentes/ReemKufi-Regular.ttf": "2e981d24acae70b64ca4bd957c21819b",
 "assets/assets/fuentes/TitanOne-Regular.ttf": "cbb4d15b35ec11cb0638570c275fdf9d",
+"assets/assets/globos.jpg": "56e526c423be725e20d97e471ea2a429",
 "assets/assets/iglesia.jpg": "1cfeb925ad9c669cc7a3124ef6ea7291",
 "assets/assets/ImagenApp/BannerInicio.jpg": "85ac94f8a49824f495dc9313785080a0",
 "assets/assets/ImagenApp/ImagenApp1.jpg": "85ac94f8a49824f495dc9313785080a0",
@@ -15,6 +16,12 @@ const RESOURCES = {
 "assets/assets/imagenGrupos/PJ.jpeg": "39806b4b4267925e8f2c8347e2c90de4",
 "assets/assets/logo.png": "2fbc477d806b81f9fee756dd9808d443",
 "assets/assets/logoAplicacion.png": "991391b339f6d5a1192da977d987649a",
+"assets/assets/pastel.jpg": "87df8e715218211214db84a19d4d6935",
+"assets/assets/paz.jpg": "5738563290e76113dd6895ee50cf6ce5",
+"assets/assets/publicaciones/1.jpg": "1cfeb925ad9c669cc7a3124ef6ea7291",
+"assets/assets/publicaciones/2.jpg": "336d3ca4a954fdbb41870d7c36e26ad2",
+"assets/assets/steve2.jpg": "70d9bb66803e5d8fa05f0224897dac5e",
+"assets/assets/steveCumple.jpg": "a2388df374d051c5f6752db5d56d63d6",
 "assets/FontManifest.json": "6bb46d326532b9c834310fec2e216d48",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "4b206514be23b09fed754adb05e05e40",
@@ -22,9 +29,9 @@ const RESOURCES = {
 "favicon.png": "8f1d26cf1b93bf71af198365819d1884",
 "icons/Icon-192.png": "0ddcdcc872f742d276813f855e0d07d3",
 "icons/Icon-512.png": "805896c266a78b4c3ca4cfc6dc35b21e",
-"index.html": "562bbb09499dabb3406ca591beda13ef",
-"/": "562bbb09499dabb3406ca591beda13ef",
-"main.dart.js": "80c9ad076cbc4964856a32f2d594e0f0",
+"index.html": "b58c3f7e5ca63e89b0d2f034da89d1a1",
+"/": "b58c3f7e5ca63e89b0d2f034da89d1a1",
+"main.dart.js": "12337d3cd0babb747b4958239f6df355",
 "manifest.json": "7db4d5376322356caab0329e7b44f78f",
 "version.json": "deb7db0b854011cd7f8725964d3eb166"
 };
